57d210a51e2b04803d68a5ca0e243fd6c97b31b4
Eduardo Brito Labrada <eblabrada@gmail.com>
2026-01-10 01:28:42 -0500

    feat: add auth service template

---
+++ server/server/auth.py
@@ -0,0 +1,127 @@
+import logging
+import os
+import grpc
+import time
+import jwt
+import datetime
+
+from concurrent import futures
+
+from protos.models_pb2 import User
+from protos.auth_pb2 import RegisterResponse, LoginResponse
+from protos.auth_pb2_grpc import AuthServiceServicer, add_AuthServiceServicer_to_server
+
+logger = logging.getLogger('socialnet.server.auth')
+# logger.setLevel(logging.INFO)
+
+
+class AuthRepository:
+    def __init__(self, node) -> None:
+        self.node = node
+
+    def exists_user(self, username: str):
+        logger.info(f'Check if username exists: {username}')
+        path = os.path.join('User', username.lower())
+        return exists(self.node, path)
+
+    def load_user(self, username: str):
+        logger.info(f'Load user: {username}')
+
+        path = os.path.join('User', username.lower())
+        user, error = load(self.node, path, User())
+
+        if error == grpc.StatusCode.NOT_FOUND:
+            return None, grpc.StatusCode.NOT_FOUND
+
+        if error:
+            return None, grpc.StatusCode.INTERNAL
+
+        return user, None
+
+    def save_user(self, user):
+        logger.info(f'Save user: {user.user_id}')
+
+        path = os.path.join('User', user.user_id.lower())
+        error = save(self.node, user, path)
+
+        if error:
+            return grpc.StatusCode.INTERNAL
+
+        return None
+
+
+class AuthService(AuthServiceServicer):
+    def __init__(self, auth_repo: AuthRepository, jwt_private_key, jwt_algorithm='HS256'):
+        self.auth_repo = auth_repo
+        self.jwt_private_key = jwt_private_key
+        self.jwt_algorithm = jwt_algorithm
+
+    def Register(self, request, context):
+        user = request.user
+
+        logger.info(f'Register an user: {user.user_id}')
+
+        exists, error = self.auth_repo.exists_user(user.user_id)
+        if exists:
+            context.abort(grpc.StatusCode.INVALID_ARGUMENT,
+                          'User already exists')
+            return RegisterResponse(success=False)
+
+        error = self.auth_repo.save_user(user)
+        if error is None:
+            logger.error('Registration failed')
+            return RegisterResponse(success=False, message='Registration failed')
+
+        logger.info('Successful registration')
+        return RegisterResponse(success=True, message='User created successfully')
+
+    def Login(self, request, context):
+        username = request.username
+        password = request.password
+
+        logger.info(f'Login user: {username}')
+
+        user, error = self.auth_repo.load_user(username)
+
+        if error:
+            if error == grpc.StatusCode.NOT_FOUND:
+                context.abort(grpc.StatusCode.PERMISSION_DENIED,
+                              'Incorrect username or password')
+            else:
+                context.abort(error, 'Error')
+
+        if user is None:
+            context.abort(grpc.StatusCode.PERMISSION_DENIED,
+                          'Incorrect username or password')
+
+        if not user or user.password_hash != password:
+            context.abort(grpc.StatusCode.PERMISSION_DENIED,
+                          'Incorrect username or password')
+
+        logger.info('Successful login')
+        token = self.gen_token(user)
+        return LoginResponse(token=token)
+
+    def gen_token(self, user):
+        expiration = (datetime.datetime.utcnow() + datetime.timedelta(hours=24)).isoformat()
+
+        payload = {
+            'user_id': user.user_id,
+            'expires': expiration
+        }
+
+        token = jwt.encode(payload, self.jwt_private_key,
+                           algorithm=self.jwt_algorithm)
+        return token
+
+
+def start_auth_service(addr, auth_repo: AuthRepository, max_workers: int = 10):
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=max_workers))
+    SECRET_KEY = os.getenv("SECRET_KEY", 'dev_secret_123')
+    add_AuthServiceServicer_to_server(
+        AuthService(auth_repo, SECRET_KEY, 'HS256'), server)
+    server.add_insecure_port(addr)
+    server.start()
+    port = str(addr).split(':')
+    logger.info(f'Auth service started on port {port[1]}')
+    server.wait_for_termination()
