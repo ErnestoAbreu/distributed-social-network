c77063bde75b414a90f0b59b5140af5c1a7fc59d
Ernesto Abreu Peraza <ernestoabreup@gmail.com>
2026-01-11 11:35:20 -0500

    fix import

--- server/server/chord/protos/chord_pb2_grpc.py
+++ server/server/chord/protos/chord_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-from server.server.chord.proto import chord_pb2 as chord__pb2
+from server.server.chord.protos import chord_pb2 as chord__pb2
 
 GRPC_GENERATED_VERSION = '1.76.0'
 GRPC_VERSION = grpc.__version__
